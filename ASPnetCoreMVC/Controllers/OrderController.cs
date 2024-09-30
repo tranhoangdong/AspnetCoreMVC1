946cb6a8de1babb319b67eb67bde6515135698e3
dong <dongtran100298@gmail.com>
2024-09-30 16:37:51 +0700

    kan-6

---
+++ ASPnetCoreMVC/Controllers/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using eShopSolution.Application.Dtos;
+using eShopSolution.Application.IService;
+using eShopSolution.Web.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public OrderController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult LoadProductTable(string name, string priceFilter, string sortColumn, string sortOrder, int? categoryId)
+        {
+            var products = _productService.GetAllProducts(name, priceFilter, sortColumn, sortOrder, categoryId);
+            var productViewModels = products.Select(p => new ProductDetailViewModel
+            {
+                ID = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+                Stock = p.Stock,
+                CategoryName = p.Category?.Name
+            }).ToList();
+            return PartialView("_ProductTableOderPartial", productViewModels);
+        }
+
+        public IActionResult Index(string name, string priceFilter, string sortColumn, string sortOrder, int? categoryId)
+        {
+            var categories = _categoryService.GetAllCategory().Select(x => new CategoryViewModel
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+            var allProductViewModel = new AllProductViewModel
+            {
+                Categories = categories,
+
+            };
+
+            return View(allProductViewModel);
+        }
+
+      
+    }
+}
