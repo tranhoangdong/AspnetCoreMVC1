6f32bd74235c28bbffc250dddfe1ba7e8cdf9cca
dong <dongtran100298@gmail.com>
2024-10-12 10:17:37 +0700

    scritp

---
+++ eShopsolution.Data/Scripts/OrderDetal.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE OrderDetail (
+    Id INT PRIMARY KEY IDENTITY(1,1),  
+    Name NVARCHAR(255) NOT NULL,       
+    Quantity INT NOT NULL,            
+    Price FLOAT NOT NULL,              
+    Tongtien FLOAT NOT NULL           
+);
