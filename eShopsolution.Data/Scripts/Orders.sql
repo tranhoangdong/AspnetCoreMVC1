2336e471f9ed6b7d3c53fab1bfef6d1507aa67d9
dong <dongtran100298@gmail.com>
2024-10-31 15:09:20 +0700

    [kan60]- Cannot save order

--- eShopsolution.Data/Scripts/Orders.sql
+++ eShopsolution.Data/Scripts/Orders.sql
@@ -3,7 +3,6 @@
     RoomAndTableId INT NOT NULL,          
     OrderTime DATETIME NOT NULL,           
     TotalAmount DECIMAL(18, 2) NOT NULL,   
-    Status NVARCHAR(50) NOT NULL,          
     Note NVARCHAR(250),                    
     FOREIGN KEY (RoomAndTableId) REFERENCES RoomAndTable(Id) 
 );
