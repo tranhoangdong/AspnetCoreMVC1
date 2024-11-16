0aaa38fa2806e1799eb14434d5e0f8ff120c404a
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-11-16 18:45:32 +0700

    [kan84]- Create a view to list all product of Book Category

---
+++ ASPnetCoreMVC/Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Web.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+
+}
